e624a6c907e3e66e028466c7e8fd99ec0196b464
Alex Robertson-Brown <alex.robertsonbrown@gmail.com>
2017-06-13 13:32:43 +0100

    rough site with final content

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@
 
   // css tricks snippet - http://css-tricks.com/snippets/jquery/smooth-scrolling/
   $(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
+    $('a[href*=\\#]:not([href=\\#])').click(function() {
       if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -28,6 +28,23 @@
   if ("ontouchstart" in document.documentElement){
     $('html').addClass('touch');
   }
+
+
+///////////////////////////////////////
+//        competition entry
+///////////////////////////////////////
+
+$('.js-competition-form').hide();
+
+$('.js-open-competition').on('click', function(e) {
+  $(this).slideUp(250);
+  $('.js-competition-form').slideDown(250, function() {
+    $('html,body').animate({
+      scrollTop: $('.js-competition-form').offset().top
+    }, 500);
+  });
+  return false;
+});
 
 
 ///////////////////////////////////////
@@ -57,78 +74,6 @@
       $(this).attr('src', newSrc);
     });
   }
-
-
-///////////////////////////////////////
-//      Parallax
-//      [ example: <div class="parallax" data-parallax-speed="0.2"> ]
-///////////////////////////////////////
-
-  // $(document).scroll(function(){
-  //   var scrolled = $(document).scrollTop();
-  //   $('.parallax').each(function(){
-  //     var speed = $(this).attr('data-parallax-speed');
-  //     var offset = $(this).offset();
-  //     var parallax = -(scrolled - offset.top) * speed ;
-  //     $(this).css('background-position', 'center ' + parallax + 'px');
-  //   });
-  // });
-
-
-///////////////////////////////////////
-//    Generic modal
-///////////////////////////////////////
-
-  var modal          = $('.js-modal'),
-      modalLaunchBtn = $('.js-open-modal'),
-      modalCloseBtn  = $('.js-close-modal');
-
-    // opens modal
-    function modalOpen(event){
-      event.preventDefault();
-      // disable scrolling on background content (doesn't work iOS)
-      $('body').addClass('disable-scroll');
-      // // open modal
-      modal.fadeIn('250', function(){
-        $(this).removeClass('is-closed').addClass('is-open');
-      });
-    }
-
-    // closes modal
-    function modalClose(event){
-      event.preventDefault();
-      // enable scrolling
-      $('body').removeClass('disable-scroll');
-      // close modal with fade
-      modal.fadeOut('250', function(){
-        $(this).removeClass('is-open').addClass('is-closed');
-      });
-    }
-
-    // launches modal when offer is clicked
-    modalLaunchBtn.on('click', function(event) {
-      modalOpen(event);
-    });
-
-    // closes modal on close icon click
-    modalCloseBtn.on('click', function(event) {
-      modalClose(event);
-    });
-
-    // closes modal on background click
-    modal.on('click', function(event) {
-      if (event.target !== this){
-        return;
-      }
-      modalClose(event);
-    });
-
-    // closes modal on escape key press
-    $(document).keyup(function(event) {
-       if (event.keyCode == 27) {
-         modalClose(event);
-        }
-    });
 
 
 ///////////////////////////////////////////////////////////////////////////////
